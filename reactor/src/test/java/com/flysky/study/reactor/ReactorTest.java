ca1e32599816f3e28d8c5f83a36a7c9ba2b2f8e7
long <1845797136@qq.com>
2019-12-01 14:25:55 +0800

    reactor初体检

---
+++ reactor/src/test/java/com/flysky/study/reactor/ReactorTest.java
@@ -0,0 +1,143 @@
+package com.flysky.study.reactor;
+
+import org.junit.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ReactorTest {
+    @Test
+    public void should_handle() {
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        Flux<String> alphabet = Flux.just(-1, 30, 13, 9, 20)
+                .handle((i, sink) -> {
+                    String letter = alphabet(i);
+                    if (letter != null)
+                        sink.next(letter);
+                });
+
+        alphabet.subscribe(System.out::println);
+    }
+
+    public String alphabet(int letterNumber) {
+        if (letterNumber < 1 || letterNumber > 26) {
+            return null;
+        }
+        int letterIndexAscii = 'A' + letterNumber - 1;
+        return "" + (char) letterIndexAscii;
+    }
+
+    @Test
+    public void should_generate() {
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3 * state);
+                    if (state == 10) sink.complete();
+                    return state + 1;
+                });
+        flux.subscribe(System.out::println);
+        System.out.println("----------------------------------");
+        Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next("3 x " + i + " = " + 3 * i);
+                    if (i == 10) sink.complete();
+                    return state;
+                })
+                .subscribe(System.out::println);
+        System.out.println("----------------------------------");
+        Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next("3 x " + i + " = " + 3 * i);
+                    if (i == 10) sink.complete();
+                    return state;
+                }, (state) -> System.out.println("state: " + state))
+                .subscribe(System.out::println)
+        ;
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+    }
+
+    @Test
+    public void should_() {
+        Flux<String> seq1 = Flux.just("foo", "bar", "foobar");
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        Flux<String> seq2 = Flux.fromIterable(iterable);
+
+        seq1.subscribe(System.out::println);
+        System.out.println("----------------------------------");
+
+        Mono<String> noData = Mono.empty();
+
+        Mono<String> data = Mono.just("foo");
+
+        Flux<Integer> numbersFromFiveToSeven = Flux.range(5, 3);
+        numbersFromFiveToSeven.subscribe(System.out::println);
+        Flux<Integer> ints = Flux.range(1, 4)
+                .map(i -> {
+                    if (i <= 3) return i;
+                    throw new RuntimeException("Got to 4");
+                });
+        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error: " + error));
+
+        System.out.println("----------------------------------");
+        Flux<Integer> ints2 = Flux.range(1, 4);
+        ints2.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> System.out.println("Done"));
+        System.out.println("----------------------------------");
+        Flux<Integer> ints3 = Flux.range(1, 4);
+        ints3.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> System.out.println("Done"),
+                sub -> sub.request(10));
+        System.out.println("----------------------------------");
+        SampleSubscriber<Integer> ss = new SampleSubscriber<Integer>();
+        Flux<Integer> ints4 = Flux.range(1, 4);
+        ints4.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> {
+                    System.out.println("Done");
+                },
+                s -> s.request(10));
+        ints4.subscribe(ss);
+        System.out.println("----------------------------------");
+        Flux.range(1, 10)
+                .doOnRequest(r -> System.out.println("request of " + r))
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    public void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    public void hookOnNext(Integer integer) {
+                        System.out.println("Cancelling after having received " + integer);
+                        cancel();
+                    }
+                });
+        System.out.println("----------------------------------");
+        System.out.println("----------------------------------");
+
+    }
+}
