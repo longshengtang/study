4d6e764b3a0100d78e6a1024e931deff173bdb36
long <1845797136@qq.com>
2020-01-02 17:50:48 +0800

    fa

--- dbunit/src/test/java/com/flysky/study/dbunit/mapper/MavenClear.java
+++ dbunit/src/test/java/com/flysky/study/dbunit/mapper/MavenClear.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class MavenClear {
     public static void main(String[] args) {
         delete(new File("/Users/longlong/tools/develop/common/maven/repository/"));
+        delete(new File("/Users/longlong/.m2/repository"));
     }
 
     private static void delete(File file){
