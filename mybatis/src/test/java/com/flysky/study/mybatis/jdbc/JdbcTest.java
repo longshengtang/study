889563319f9de4c58b6ddd23aa2c2578ee3e835a
long <1845797136@qq.com>
2022-08-09 14:42:21 +0800

    JDBC通过Driver获取数据库连接

--- mybatis/src/test/java/com/flysky/study/mybatis/jdbc/JdbcTest.java
+++ mybatis/src/test/java/com/flysky/study/mybatis/jdbc/JdbcTest.java
@@ -18,4 +18,20 @@ public class JdbcTest {
             System.out.println("resultSet.getObject(1) = " + resultSet.getObject(1));
         }
     }
+    @Test
+    public void testGetConnectByDriver() throws SQLException {
+        String url="jdbc:mysql://10.1.1.11:3306/flysky";
+        String user="root";
+        String password="123456";
+        Driver d = DriverManager.getDriver(url);
+        java.util.Properties info = new java.util.Properties();
+        info.put("user", user);
+        info.put("password", password);
+        Connection connection = d.connect(url, info);
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from sys_user");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println("resultSet.getObject(1) = " + resultSet.getObject(1));
+        }
+    }
 }
